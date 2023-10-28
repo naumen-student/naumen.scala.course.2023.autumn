5ff1e231025f4154a1a68702348dc2bd77f57d4e
maxoriy <anohin257@gmail.com>
2023-10-28 22:20:28 +0500

    task1

---
+++ homeworks/homework_1/task_3/src/main/scala/Main.scala
@@ -0,0 +1,12 @@
+object Main extends App {
+  def greeting(name: String): String = s"Hello, scala! This is ${name}."
+
+  val name: String = "Maxim Anokhin"
+  val reversedName: String = name.reverse
+  println(greeting(name))
+
+  println(greeting(name).replace("Hello", "Guten tag"))
+
+  println(greeting(reversedName).replace("Hello", "Guten tag"))
+
+}
