2ee392e3a9ae70f4e3773e2cbaaee1cb5cbf0def
v.d.zubkov <v.d.zubkov@tinkoff.ru>
2023-10-29 21:43:46 +0500

    homework_1 - Зубков Владислав

---
+++ homeworks/homework_1/task_3/Main.scala
@@ -0,0 +1,20 @@
+object Main extends App {
+  val name = "Vladislav Zubkov"
+
+  val enGreeting = "Hello"
+  val spGreeting = "Hola"
+  val gerGreeting = "Guten tag"
+
+  def greet(greeting: String, name: String): Unit = {
+    println(s"$greeting Scala! This is $name")
+  }
+
+  greet(enGreeting, name)
+  greet(spGreeting, name)
+  greet(gerGreeting, name)
+
+  greet(gerGreeting, name.reverse)
+  greet(spGreeting, name.reverse)
+  greet(enGreeting, name.reverse)
+
+}
