3ec4e6770382fe78f2ab2f7f7d26faef1372a261
Alina200207 <alina_m2002@list.ru>
2023-10-29 18:34:44 +0500

    homework2

--- homeworks/homework_2/src/test/scala/Test.scala
+++ homeworks/homework_2/src/test/scala/Test.scala
@@ -1,12 +1,40 @@
+import Exercises.Vector2D
 import utest._
 
 object Test extends TestSuite{
 
-    val tests = Tests{
+    val tests: Tests = Tests{
         'test_divBy3Or7 - {
             assert(Exercises.divBy3Or7(1, 3) == Seq(3))
             assert(Exercises.divBy3Or7(5, 9) == Seq(6, 7, 9))
             assert(Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33, 35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93, 96, 98, 99))
+        }
+        'test_sumOfDivBy3Or5 - {
+            assert(Exercises.sumOfDivBy3Or5(1, 3) == 3)
+            assert(Exercises.sumOfDivBy3Or5(3, 9) == 23)
+            assert(Exercises.sumOfDivBy3Or5(0, 50) == 593)
+        }
+        'test_primeFactor - {
+            assert(Exercises.primeFactor(23) == Seq(23))
+            assert(Exercises.primeFactor(180) == Seq(2, 3, 5))
+            assert(Exercises.primeFactor(98) == Seq(2, 7))
+            assert(Exercises.primeFactor(1) == Seq())
+        }
+        'test_sumScalars - {
+            assert(Exercises.sumScalars(Vector2D(2, 3), Vector2D(1, 4), Vector2D(1, 2), Vector2D(3, 2)) == 21)
+            assert(Exercises.sumScalars(Vector2D(0, 0), Vector2D(1, 4), Vector2D(1, 2), Vector2D(0, 2)) == 4)
+            assert(Exercises.sumScalars(Vector2D(2.5, 3.5), Vector2D(1, 4), Vector2D(1, 2), Vector2D(3.7, 2.4)) == 25)
+        }
+        'test_sumCosines - {
+            assert(Exercises.sumCosines(Vector2D(3, 4), Vector2D(4, 3), Vector2D(4, 3), Vector2D(3, 4)) == 1.92)
+            assert(Exercises.sumCosines(Vector2D(0, 3), Vector2D(1, 0), Vector2D(3, 4), Vector2D(0, 2)) == 0.8)
+        }
+        'test_sortByHeavyWeight - {
+            assert(Exercises.sortByHeavyWeight() == Seq("Tin", "Platinum", "Nickel", "Aluminum", "Titanium", "Lead", "Sodium", "Uranium",
+                "Gold", "Tungsten", "Zirconium", "Chrome", "Iron", "Copper", "Silver", "Plutonium", "Cobalt", "Cesium", "Calcium", "Lithium",
+                "Magnesium", "Potassium", "Graphite"))
+            assert(Exercises.sortByHeavyWeight(Map("Aluminum" -> (5, 2.6889), "Graphite" -> (8, 2.1), "Iron" -> (9, 7.874),
+                "Gold" -> (2, 19.32))) == Seq("Gold", "Aluminum", "Graphite", "Iron"))
         }
     }
 }
