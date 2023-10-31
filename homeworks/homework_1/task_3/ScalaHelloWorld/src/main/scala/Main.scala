f727acbd069d069047ec3ed0a9835cf48ae561d2
Danil_Titarenko <danil.titarenko@gmail.com>
2023-10-31 20:31:28 +0500

    task 3 updated with all project files

--- homeworks/homework_1/task_3/ScalaHelloWorld/src/main/scala/Main.scala
+++ homeworks/homework_1/task_3/ScalaHelloWorld/src/main/scala/Main.scala
@@ -1,19 +1,12 @@
-object Main extends App {
+object Main {
+  def main(args: Array[String]): Unit = {
+    val greetings: List[String] = List("Hola", "Hello", "Salam")
+    val name: String = "Danil Titarenko"
+    val reversedName: String = name.reverse
+    val names: List[String] = List(name, reversedName)
+    greetings.foreach(g => names.foreach(n => greeting(g, n)))
+  }
   def greeting(greet: String, name: String): Unit = {
     println(s"$greet Scala! This is $name")
   }
-
-  val hola: String = "Hola"
-  val hello: String = "Hello"
-  val salam: String = "Salam"
-  val name: String = "Danil Titarenko"
-  val reversedName: String = name.reverse
-
-  greeting(hello, name)
-  greeting(hola, name)
-  greeting(salam, name)
-
-  greeting(hello, reversedName)
-  greeting(hola, reversedName)
-  greeting(salam, reversedName)
 }
