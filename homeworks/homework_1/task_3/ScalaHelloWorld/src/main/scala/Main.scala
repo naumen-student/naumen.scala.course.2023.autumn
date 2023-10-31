584c9a39659e187727edfd003b782910a3c5d8c8
Danil_Titarenko <danil.titarenko@gmail.com>
2023-10-31 18:50:06 +0500

    task 3

---
+++ homeworks/homework_1/task_3/ScalaHelloWorld/src/main/scala/Main.scala
@@ -0,0 +1,19 @@
+object Main extends App {
+  def greeting(greet: String, name: String): Unit = {
+    println(s"$greet Scala! This is $name")
+  }
+
+  val hola: String = "Hola"
+  val hello: String = "Hello"
+  val salam: String = "Salam"
+  val name: String = "Danil Titarenko"
+  val reversedName: String = name.reverse
+
+  greeting(hello, name)
+  greeting(hola, name)
+  greeting(salam, name)
+
+  greeting(hello, reversedName)
+  greeting(hola, reversedName)
+  greeting(salam, reversedName)
+}
