cb0277fb8d3ab1fe2d80b3ca6fe1006ba71aea7e
SergeyNovgorodskiy <ns-7@bk.ru>
2023-11-03 23:07:42 +0500

    1

---
+++ homeworks/homework_1 - Новгородский Сергей/task_3/Task3/SbtExampleProject/src/main/scala/example/Main.scala
@@ -0,0 +1,14 @@
+package example
+
+object Main extends App{
+  val a = ("Hello Scala! This is ")
+  val b = ("Halo Scala! This is ")
+  val c = ("Guten tag Scala! This is ")
+  val d = ("$Sergey Novgorodskiy".reverse)
+
+  println(a + d)
+  println(b + d)
+  println(c + d)
+
+
+}
