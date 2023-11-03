f0fbcf112196677e0881e1111e1576f5e61e799f
Dan <kondratovdany@gmail.com>
2023-11-03 21:35:49 +0300

    Task1_fixed

--- homeworks/homework_1/task_3/SbtExampleProject/src/main/scala/example/Main.scala
+++ homeworks/homework_1/task_3/SbtExampleProject/src/main/scala/example/Main.scala
@@ -4,7 +4,7 @@ object Main {
   def main(args: Array[String]): Unit = {
     val name = "Daniil"
     val func1: String => String = _ + s" Scala! This is $name"
-    val func2: String => String = _ + s" Scala! This is $name.reverse"
+    val func2: String => String = _ + s" Scala! This is ${name.reverse}"
     val words = List("Hola", "Guten Tag", "Hello")
     println(words.map(func1(_)).mkString("\n"))
     println(words.map(func2(_)).mkString("\n"))
