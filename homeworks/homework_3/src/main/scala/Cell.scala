057c0794cf095a3acc30f7a51aef36f64ac5313a
Igor Krutikov <krutickovig@yandex.ru>
2023-11-04 23:06:42 +0500

    hw3 is done

---
+++ homeworks/homework_3/src/main/scala/Cell.scala
@@ -0,0 +1,24 @@
+trait Cell
+
+class EmptyCell extends Cell {
+    override def toString: String = "empty"
+}
+
+class NumberCell(number: Int) extends Cell {
+    override def toString: String = number.toString
+}
+
+class StringCell(string: String) extends Cell {
+    override def toString: String = string
+}
+
+class ReferenceCell(x: Int, y:Int, table: Table) extends Cell {
+    @scala.annotation.tailrec
+    private def chainToString(visited: Set[Cell] = Set.empty[Cell]):String = table.getCell(x, y) match {
+        case Some(ref:ReferenceCell) => if (visited.contains(ref)) "cyclic" else ref.chainToString(visited++Set(ref))
+        case Some(cell:Cell) => cell.toString
+        case None => "OutOfRange"
+    }
+
+    override def toString: String = chainToString()
+}
---
+++ homeworks/homework_3/src/main/scala/Table.scala
@@ -0,0 +1,9 @@
+class Table(height: Int, width: Int) {
+    private val data: Array[Cell] = Array.fill[Cell](height * width)(new EmptyCell)
+
+    private def toInd(ix: Int, iy: Int): Option[Int] = Some(ix + iy * width).filter(inRange)
+    private def inRange(ind: Int): Boolean = 0 <= ind && ind < data.length
+    def getCell(ix: Int, iy: Int): Option[Cell] = toInd(ix,iy).map(data(_))
+
+    def setCell(ix: Int, iy: Int, cell: Cell): Unit = toInd(ix,iy).map(data(_) = cell)
+}
