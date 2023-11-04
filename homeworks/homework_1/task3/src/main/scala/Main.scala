504afc18e388ae130eda1df2f5893664c5709278
EGREX <kirilmikov591@gmail.com>
2023-11-04 10:05:08 +0500

    Revert "add task3"
    This reverts commit cd4d2c5ab29198a159f5ee0b3402bf27831e658f.

--- homeworks/homework_1/task3/src/main/scala/Main.scala
+++
@@ -1,8 +0,0 @@
-object Main {
-  def main(args: Array[String]): Unit = {
-    val a = "Kirill Mikov"
-    println("Hello World! This is " + a)
-    println("Hola World! This is " + a.reverse)
-    println(("Guten tag World! This is " + a.reverse).reverse)
-  }
-}
