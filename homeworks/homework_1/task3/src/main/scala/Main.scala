cd4d2c5ab29198a159f5ee0b3402bf27831e658f
EGREX <kirilmikov591@gmail.com>
2023-11-04 09:51:24 +0500

    add task3

---
+++ homeworks/homework_1/task3/src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+    val a = "Kirill Mikov"
+    println("Hello World! This is " + a)
+    println("Hola World! This is " + a.reverse)
+    println(("Guten tag World! This is " + a.reverse).reverse)
+  }
+}
