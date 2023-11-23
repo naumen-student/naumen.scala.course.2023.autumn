fb91d83a486e1da69dbbe0bdfbdae9dcf2c7ce1e
d.zaichkin <d.zaichkin@tinkoff.ru>
2023-11-23 21:15:57 +0500

    homework_3

---
+++ homeworks/homework_3/src/main/scala/Table.scala
@@ -0,0 +1,10 @@
+import scala.collection.mutable.ArrayOps
+import scala.util.Try
+
+class Table(width: Int, height: Int) {
+
+  private val storage: Array[Cell] = Array.fill[Cell](width * height)(EmptyCell)
+  def getCell(ix: Int, iy: Int): Option[Cell] = Try(storage(ix + iy * width)).toOption
+
+  def setCell(ix: Int, iy: Int, cell: Cell): Unit = storage.update(ix + iy * width, cell)
+}
