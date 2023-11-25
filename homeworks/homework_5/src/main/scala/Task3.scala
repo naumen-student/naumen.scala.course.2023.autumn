408ae1aac776acd69b28f76169f27e458babbee3
Maria <masha0352@gmail.com>
2023-11-25 18:37:57 +0500

    homework_5

--- homeworks/homework_5/src/main/scala/Task3.scala
+++ homeworks/homework_5/src/main/scala/Task3.scala
@@ -25,8 +25,21 @@ object Task3 extends App {
   case class Count(word: String, count: Int)
   case class WordsCount(count: Seq[Count])
   object WordsCount {
-    implicit val monoid: Monoid[WordsCount] = ???
+    implicit val monoid: Monoid[WordsCount] = new Monoid[WordsCount] {
+      override def empty: WordsCount = WordsCount(Seq.empty[Count])
+
+      override def combine(x: WordsCount, y: WordsCount): WordsCount = {
+        WordsCount((x.count ++ y.count)
+          .groupBy { case Count(word, _) => word }
+          .mapValues(a => a.map(_.count).sum)
+          .toSeq
+          .map { case (word, count) => Count(word, count) }
+        )
+      }
+    }
   }
 
-  def countWords(lines: Vector[String]): WordsCount = ???
+  def countWords(lines: Vector[String]): WordsCount =
+    Await.result(mapReduce(lines.flatMap(_.split(" ")))
+    (word => WordsCount(Seq(Count(word, 1)))), 1.second)
 }
