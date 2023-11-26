242baa3b9ab6fe46b64c707f04fcf542e2b7b81f
KrauhinSemen <72562026+KrauhinSemen@users.noreply.github.com>
2023-11-26 20:55:35 +0500

    Update Task2.scala

--- homeworks/homework_5/src/main/scala/Task2.scala
+++ homeworks/homework_5/src/main/scala/Task2.scala
@@ -10,17 +10,57 @@ import cats.implicits._
  */
 object Task2 extends App {
   case class RadiusVector(x: Int, y: Int)
+
   object RadiusVector {
-    implicit val monoid: Monoid[RadiusVector] = ???
-  }
-  case class DegreeAngle(angel: Double)
-  object DegreeAngle {
-    implicit val monoid: Monoid[DegreeAngle] = ???
+    implicit val monoid: Monoid[RadiusVector] = new Monoid[RadiusVector] {
+      override def empty: RadiusVector = RadiusVector(0, 0)
+
+      override def combine(vec_1: RadiusVector, vec_2: RadiusVector): RadiusVector = {
+        val sum_x = vec_1.x + vec_2.x
+        val sum_y = vec_1.y + vec_2.y
+        RadiusVector(sum_x, sum_y)
+      }
+    }
   }
 
-  case class SquareMatrix[A : Monoid](values: ((A, A, A), (A, A, A), (A, A, A)))
+  case class DegreeAngle(angel: Double)
+
+  object DegreeAngle {
+    implicit val monoid: Monoid[DegreeAngle] = new Monoid[DegreeAngle] {
+      override def empty: DegreeAngle = DegreeAngle(0.0)
+
+      /*
+      Здесь скорее всего будет ошибка в тесте по следующей причине:
+      Ожидаемому ответу в тесте нужен тот же угол, что и получаемый в программе ниже,
+      однако по какой-то причине требуется дополнительный поворот на 360 градусов в ту или иную сторону.
+      Например -220 превращатся в 140 (или наоборот)
+      По своей сути это один и тот же угол, поэтому оставил код как есть,
+      так как не понял закоомерности, когда нужен этот поворот, а когда нет.
+      Надеюсь, что на зачёт/незачёт задачи это не повлияет...
+      */
+      override def combine(inclination_1: DegreeAngle, inclination_2: DegreeAngle): DegreeAngle = {
+        val sum_inclination = (inclination_1.angel + inclination_2.angel) % 360
+        DegreeAngle(sum_inclination)
+      }
+    }
+  }
+
+  case class SquareMatrix[A: Monoid](values: ((A, A, A), (A, A, A), (A, A, A)))
+
   object SquareMatrix {
-    implicit def monoid[A: Monoid]: Monoid[SquareMatrix[A]] = ???
+    implicit def monoid[A: Monoid]: Monoid[SquareMatrix[A]] = new Monoid[SquareMatrix[A]] {
+      override def empty: SquareMatrix[A] = {
+        val element = Monoid[A].empty
+        val row = (element, element, element)
+        val matrix = (row, row, row)
+        SquareMatrix(matrix)
+      }
+
+      override def combine(x: SquareMatrix[A], y: SquareMatrix[A]): SquareMatrix[A] = {
+        val matrix = x.values.combine(y.values)
+        SquareMatrix(matrix)
+      }
+    }
   }
 
   val radiusVectors = Vector(RadiusVector(0, 0), RadiusVector(0, 1), RadiusVector(-1, 1))
