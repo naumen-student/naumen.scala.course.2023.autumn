ffd56fe1efc28e3cb53019b67d9affb86b41646f
cmbbigis <gavrilov.nikita@urfu.me>
2023-12-01 16:50:55 +0500

    done

--- homeworks/homework_5/src/main/scala/Task1.scala
+++ homeworks/homework_5/src/main/scala/Task1.scala
@@ -45,9 +45,18 @@ object Task1 extends App {
   }
 
   object ShowInstance {
-    implicit val catShow: Show[Cat] = ???
+    implicit val catShow: Show[Cat] = {
+      case VeryLittleCat(name) => s"очень маленький кот $name"
+      case LittleCat(name) => s"маленький кот $name"
+      case NormalCat(name) => s"кот $name"
+      case BigCat(name) => s"большой кот $name"
+      case VeryBigCat(name) => s"очень большой кот $name"
+    }
 
-    implicit def boxShow[A: Show]: Show[Box[A]] = ???
+    implicit def boxShowA[A](implicit showA: Show[A]): Show[Box[A]] = {
+      case BoxWith(value) => s"${showA.show(value)} в коробке"
+      case EmptyBox => "пустая коробка"
+    }
   }
 
   object ShowSyntax {
--- homeworks/homework_5/src/main/scala/Task2.scala
+++ homeworks/homework_5/src/main/scala/Task2.scala
@@ -10,17 +10,45 @@ import cats.implicits._
  */
 object Task2 extends App {
   case class RadiusVector(x: Int, y: Int)
+
   object RadiusVector {
-    implicit val monoid: Monoid[RadiusVector] = ???
+    implicit val monoid: Monoid[RadiusVector] = new Monoid[RadiusVector] {
+      def empty: RadiusVector = RadiusVector(0, 0)
+
+      def combine(x: RadiusVector, y: RadiusVector): RadiusVector = RadiusVector(x.x + y.x, x.y + y.y)
+    }
   }
-  case class DegreeAngle(angel: Double)
+
+  case class DegreeAngle(private val _angle: Double) {
+    val angle: Double = {
+      val correctedAngle = _angle % 360
+      if (correctedAngle < 0) correctedAngle + 360 else correctedAngle
+    }
+  }
   object DegreeAngle {
-    implicit val monoid: Monoid[DegreeAngle] = ???
+    implicit val monoid: Monoid[DegreeAngle] = new Monoid[DegreeAngle] {
+      def empty: DegreeAngle = DegreeAngle(0)
+
+      def combine(x: DegreeAngle, y: DegreeAngle): DegreeAngle = DegreeAngle((x.angle + y.angle) % 360)
+    }
   }
 
   case class SquareMatrix[A : Monoid](values: ((A, A, A), (A, A, A), (A, A, A)))
+
   object SquareMatrix {
-    implicit def monoid[A: Monoid]: Monoid[SquareMatrix[A]] = ???
+    implicit def monoid[A: Monoid]: Monoid[SquareMatrix[A]] = new Monoid[SquareMatrix[A]] {
+      def empty: SquareMatrix[A] = SquareMatrix(((Monoid[A].empty, Monoid[A].empty, Monoid[A].empty),
+        (Monoid[A].empty, Monoid[A].empty, Monoid[A].empty),
+        (Monoid[A].empty, Monoid[A].empty, Monoid[A].empty)))
+
+      def combine(x: SquareMatrix[A], y: SquareMatrix[A]): SquareMatrix[A] = {
+        val ((x11, x12, x13), (x21, x22, x23), (x31, x32, x33)) = x.values
+        val ((y11, y12, y13), (y21, y22, y23), (y31, y32, y33)) = y.values
+        SquareMatrix(((x11 |+| y11, x12 |+| y12, x13 |+| y13),
+          (x21 |+| y21, x22 |+| y22, x23 |+| y23),
+          (x31 |+| y31, x32 |+| y32, x33 |+| y33)))
+      }
+    }
   }
 
   val radiusVectors = Vector(RadiusVector(0, 0), RadiusVector(0, 1), RadiusVector(-1, 1))
