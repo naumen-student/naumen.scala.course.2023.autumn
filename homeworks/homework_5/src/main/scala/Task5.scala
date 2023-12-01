94cdce8bb8184d43e77265c1ed2f7843a989ecf6
Christmasy <istominachr2002@gmail.com>
2023-12-01 09:44:49 +0500

    homework_5

--- homeworks/homework_5/src/main/scala/Task5.scala
+++ homeworks/homework_5/src/main/scala/Task5.scala
@@ -15,11 +15,37 @@ object Task5 extends App {
     def isError: Boolean
   }
   object MyEither {
-    def apply[A](value: A): MyEither[Nothing, A] = ???
-    def error[E, A](error: E): MyEither[E, A] = ???
-    def possibleError[A](f: => A): MyEither[Throwable, A] = ???
+    case class MyRight[+E, +A](value: A) extends MyEither[E, A] {
+      def isError: Boolean = false
+    }
 
-    implicit def myEitherMonad[E]: MonadError[MyEither, E] = ???
+    case class MyLeft[+E, +A](error: E) extends MyEither[E, A] {
+      def isError: Boolean = true
+    }
+    def apply[A](value: A): MyEither[Nothing, A] = MyRight(value)
+
+    def error[E, A](error: E): MyEither[E, A] = MyLeft(error)
+
+    def possibleError[A](f: => A): MyEither[Throwable, A] = Try(f).fold(
+      thr => MyLeft(thr),
+      value => MyRight(value)
+    )
+
+    implicit def myEitherMonad[E]: MonadError[MyEither, E] = new MonadError[MyEither, E] {
+      override def pure[A](value: A): MyEither[E, A] = MyRight(value)
+
+      override def flatMap[A, B](fa: MyEither[E, A])(f: A => MyEither[E, B]): MyEither[E, B] = fa match {
+        case MyRight(value) => f(value)
+        case MyLeft(error) => MyLeft(error)
+      }
+
+      override def raiseError[A](fa: MyEither[E, A])(error: => E): MyEither[E, A] = MyLeft(error)
+
+      override def handleError[A](fa: MyEither[E, A])(handle: E => A): MyEither[E, A] = fa match {
+        case MyRight(value) => MyRight(value)
+        case MyLeft(error) => MyRight(handle(error))
+      }
+    }
   }
 
   object MyEitherSyntax {
