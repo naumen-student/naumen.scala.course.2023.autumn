8a1ae2402250588f6b729f7164e7a13e4f7cf652
polinabubb <polinabubb@gmail.com>
2023-12-01 17:00:16 +0500

    homework_5

--- homeworks/homework_5/src/main/scala/Task5.scala
+++ homeworks/homework_5/src/main/scala/Task5.scala
@@ -9,20 +9,51 @@ import scala.util.{Failure, Success, Try}
   3) Написать функции apply, error, possibleError
  */
 object Task5 extends App {
+
   import Task4.MonadError
 
   sealed trait MyEither[+E, +A] {
     def isError: Boolean
   }
-  object MyEither {
-    def apply[A](value: A): MyEither[Nothing, A] = ???
-    def error[E, A](error: E): MyEither[E, A] = ???
-    def possibleError[A](f: => A): MyEither[Throwable, A] = ???
 
-    implicit def myEitherMonad[E]: MonadError[MyEither, E] = ???
+  object MyEither {
+
+    private case class MyLeft[+E, +A](error: E) extends MyEither[E, Nothing] {
+      def isError: Boolean = true
+    }
+
+    private case class MyRight[+E, +A](value: A) extends MyEither[Nothing, A] {
+      def isError: Boolean = false
+    }
+
+    def apply[A](value: A): MyEither[Nothing, A] = MyRight(value)
+
+    def error[E, A](error: E): MyEither[E, A] = MyLeft(error)
+
+    def possibleError[A](f: => A): MyEither[Throwable, A] = Try(f) match {
+      case Success(value) => MyRight(value)
+      case Failure(exc) => MyLeft(exc)
+    }
+
+    implicit def myEitherMonad[E]: MonadError[MyEither, E] = new MonadError[MyEither, E] {
+      def pure[A](value: A): MyEither[E, A] = MyRight(value)
+
+      def flatMap[A, B](fa: MyEither[E, A])(f: A => MyEither[E, B]): MyEither[E, B] = fa match {
+        case MyRight(value) => f(value)
+        case MyLeft(exc) => MyLeft(exc)
+      }
+
+      def raiseError[A](fa: MyEither[E, A])(error: => E): MyEither[E, A] = MyLeft(error)
+
+      def handleError[A](fa: MyEither[E, A])(handle: E => A): MyEither[E, A] = fa match {
+        case MyRight(value) => MyRight(value)
+        case MyLeft(exc) => MyRight(handle(exc))
+      }
+    }
   }
 
   object MyEitherSyntax {
+
     implicit class MyEitherOps[E, A](val either: MyEither[E, A]) {
       def flatMap[B](f: A => MyEither[E, B]): MyEither[E, B] =
         MyEither.myEitherMonad[E].flatMap(either)(f)
@@ -32,5 +63,7 @@ object Task5 extends App {
       def handleError(f: E => A): MyEither[E, A] =
         MyEither.myEitherMonad.handleError(either)(f)
     }
+
   }
+
 }
