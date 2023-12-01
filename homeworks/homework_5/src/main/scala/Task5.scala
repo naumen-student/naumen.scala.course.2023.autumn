bf83e40f14145e8bcfb758857acd64245d8b9a72
Ильдар Юмагулов <iyumagulov@naumen.ru>
2023-12-01 13:38:02 +0500

    homework_5

--- homeworks/homework_5/src/main/scala/Task5.scala
+++ homeworks/homework_5/src/main/scala/Task5.scala
@@ -1,4 +1,6 @@
-import scala.util.{Failure, Success, Try}
+import cats.MonadError
+
+import scala.util.{Failure, Left, Success, Try}
 
 /*
   Задание №5
@@ -9,17 +11,46 @@ import scala.util.{Failure, Success, Try}
   3) Написать функции apply, error, possibleError
  */
 object Task5 extends App {
+
   import Task4.MonadError
 
-  sealed trait MyEither[+E, +A] {
+  sealed trait MyEither[+E, +R] {
     def isError: Boolean
   }
-  object MyEither {
-    def apply[A](value: A): MyEither[Nothing, A] = ???
-    def error[E, A](error: E): MyEither[E, A] = ???
-    def possibleError[A](f: => A): MyEither[Throwable, A] = ???
 
-    implicit def myEitherMonad[E]: MonadError[MyEither, E] = ???
+  object MyEither {
+    case class Left[+E, +R](error: E) extends MyEither[E, R] {
+      override def isError: Boolean = true
+    }
+
+    case class Right[+E, +R](result: R) extends MyEither[E, R] {
+      override def isError: Boolean = false
+    }
+
+    def apply[A](value: A): MyEither[Nothing, A] = Right(value)
+
+    def error[E, A](error: E): MyEither[E, A] = Left(error)
+
+    def possibleError[A](f: => A): MyEither[Throwable, A] = Try(f) match {
+      case Success(result) => Right(result)
+      case Failure(error) => Left(error)
+    }
+
+    implicit def myEitherMonad[E]: MonadError[MyEither, E] = new MonadError[MyEither, E] {
+      override def raiseError[R](fa: MyEither[E, R])(exp: => E): MyEither[E, R] = Left(exp)
+
+      override def pure[R](result: R): MyEither[E, R] = Right(result)
+
+      override def flatMap[A, B](fa: MyEither[E, A])(f: A => MyEither[E, B]): MyEither[E, B] = fa match {
+        case Right(result) => f(result)
+        case Left(error) => Left(error)
+      }
+
+      override def handleError[R](fa: MyEither[E, R])(handle: E => R): MyEither[E, R] = fa match {
+        case Right(result) => Right(result)
+        case Left(error) => Right(handle(error))
+      }
+    }
   }
 
   object MyEitherSyntax {
