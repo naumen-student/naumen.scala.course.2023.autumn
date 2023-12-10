515f75097dead35753614c8607df16fcebffa60f
Aleksandra <92291651+Aleksa4991@users.noreply.github.com>
2023-12-11 02:51:21 +0500

    homework_3-Калугина Александра
    homework_3-Калугина Александра

---
+++ homeworks/homework_3/src/main/scala/Table.scala
@@ -0,0 +1,19 @@
+class Table(width: Int, height:  Int)
+{
+  val table: Array[Array[Cell]] = Array.tabulate(width, height)((x, y) => new EmptyCell)
+
+  def setCell(i_x: Int, i_y: Int, cell: Cell): Unit =
+  {
+    if (i_x >= 0 && i_y >= 0 && i_x < width && i_y < height) {
+      table(i_x)(i_y) = cell
+    }
+  }
+  def getCell(i_x: Int, i_y: Int): Option[Cell] =
+  {
+    if (i_x >= 0 && i_y >= 0 && i_x < width && i_y < height) {
+      Some(table(i_x)(i_y))
+    } else {
+      None
+    }
+  }
+}
