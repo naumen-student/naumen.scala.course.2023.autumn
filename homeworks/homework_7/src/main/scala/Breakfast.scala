b085251e7115e80208e926917e3df9b92c0afc33
SoftOwl256 <anna.sok.03@mail.ru>
2023-12-14 21:46:37 +0500

    dinner

--- homeworks/homework_7/src/main/scala/Breakfast.scala
+++ homeworks/homework_7/src/main/scala/Breakfast.scala
@@ -2,7 +2,7 @@ package ru.dru
 
 import zio.CanFail.canFailAmbiguous1
 import zio.{Duration, Exit, Fiber, Scope, ZIO, ZIOApp, ZIOAppArgs, ZIOAppDefault, durationInt}
-
+import zio.{Ref, ZIO, Clock}
 import java.time.LocalDateTime
 import scala.concurrent.TimeoutException
 
@@ -29,12 +29,42 @@ object Breakfast extends ZIOAppDefault {
    * @param teaBrewingTime время заваривания чая
    * @return Мапу с информацией о том, когда завершился очередной этап (eggs, water, saladWithSourCream, tea)
    */
+
+
+
   def makeBreakfast(eggsFiringTime: Duration,
                     waterBoilingTime: Duration,
                     saladInfoTime: SaladInfoTime,
-                    teaBrewingTime: Duration): ZIO[Any, Throwable, Map[String, LocalDateTime]] = ???
+                    teaBrewingTime: Duration): ZIO[Any, Throwable, Map[String, LocalDateTime]] = {
 
+    val makeEggs = ZIO.sleep(eggsFiringTime)
+    val boilWater = ZIO.sleep(waterBoilingTime)
+    val cutCucumbers = ZIO.sleep(saladInfoTime.cucumberTime)
+    val cutTomatoes = ZIO.sleep(saladInfoTime.tomatoTime)
+    val addSourCream = ZIO.unit
+    val brewTea = ZIO.sleep(teaBrewingTime)
 
+    for {
+      times <- Ref.make(Map.empty[String, LocalDateTime])
+      currentTime = Clock.currentDateTime.map(_.toLocalDateTime)
+      _ <- (for {
+        _ <- makeEggs *> currentTime.flatMap(time => times.update(_ + ("eggs" -> time)))
+        _ <- boilWater *> currentTime.flatMap(time => times.update(_ + ("water" -> time)))
+        _ <- (for {
+          _ <- cutCucumbers *> currentTime.flatMap(time => times.update(_ + ("cucumbers" -> time)))
+          _ <- cutTomatoes *> currentTime.flatMap(time => times.update(_ + ("tomatoes" -> time)))
+          _ <- addSourCream *> currentTime.flatMap(time => times.update(_ + ("saladWithSourCream" -> time)))
+        } yield ()) *> currentTime.flatMap(time => times.update(_ + ("salad" -> time)))
+      } yield ()).zipPar(
+        brewTea *> currentTime.flatMap(time => times.update(_ + ("tea" -> time)))
+      )
+      finalTimes <- times.get
+    } yield finalTimes
+  }
+
+  // Происходит вот это, что делать - не знаю :с Написала что-то, но тесты не запускаются
+  //Error: Could not find or load main class ru.dru.BreakfastTest
+  //Caused by: java.lang.ClassNotFoundException: ru.dru.BreakfastTest
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ZIO.succeed(println("Done"))
 
