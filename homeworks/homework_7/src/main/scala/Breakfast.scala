35c7a7590243c17a5456b1739da459895f0f51f0
Alina200207 <alina_m2002@list.ru>
2023-12-16 16:48:25 +0500

    homework 7

--- homeworks/homework_7/src/main/scala/Breakfast.scala
+++ homeworks/homework_7/src/main/scala/Breakfast.scala
@@ -1,7 +1,7 @@
 package ru.dru
 
 import zio.CanFail.canFailAmbiguous1
-import zio.{Duration, Exit, Fiber, Scope, ZIO, ZIOApp, ZIOAppArgs, ZIOAppDefault, durationInt}
+import zio.{Duration, Exit, Fiber, Scope, UIO, ZIO, ZIOApp, ZIOAppArgs, ZIOAppDefault, durationInt}
 
 import java.time.LocalDateTime
 import scala.concurrent.TimeoutException
@@ -15,26 +15,47 @@ object Breakfast extends ZIOAppDefault {
    * Функция должна эмулировать приготовление завтрака. Продолжительные операции необходимо эмулировать через ZIO.sleep.
    * Правила приготовления следующие:
    *  1. Нобходимо вскипятить воду (время кипячения waterBoilingTime)
-   *  2. Параллельно с этим нужно жарить яичницу eggsFiringTime
-   *  3. Параллельно с этим готовим салат:
-   *    * сначала режим  огурцы
-   *    * после этого режим помидоры
-   *    * после этого добавляем в салат сметану
-   *  4. После того, как закипит вода необходимо заварить чай, время заваривания чая teaBrewingTime
-   *  5. После того, как всё готово, можно завтракать
+   *     2. Параллельно с этим нужно жарить яичницу eggsFiringTime
+   *     3. Параллельно с этим готовим салат:
+   *     * сначала режим  огурцы
+   *     * после этого режим помидоры
+   *     * после этого добавляем в салат сметану
+   *     4. После того, как закипит вода необходимо заварить чай, время заваривания чая teaBrewingTime
+   *     5. После того, как всё готово, можно завтракать
    *
-   * @param eggsFiringTime время жарки яичницы
+   * @param eggsFiringTime   время жарки яичницы
    * @param waterBoilingTime время кипячения воды
-   * @param saladInfoTime информация о времени для приготовления салата
-   * @param teaBrewingTime время заваривания чая
+   * @param saladInfoTime    информация о времени для приготовления салата
+   * @param teaBrewingTime   время заваривания чая
    * @return Мапу с информацией о том, когда завершился очередной этап (eggs, water, saladWithSourCream, tea)
    */
   def makeBreakfast(eggsFiringTime: Duration,
                     waterBoilingTime: Duration,
                     saladInfoTime: SaladInfoTime,
-                    teaBrewingTime: Duration): ZIO[Any, Throwable, Map[String, LocalDateTime]] = ???
+                    teaBrewingTime: Duration): ZIO[Any, Throwable, Map[String, LocalDateTime]] = for {
+    waterBoil <- boilWater(waterBoilingTime).fork
+    eggsFry <- fryEggs(eggsFiringTime).fork
+    saladMaker <- makeSalad(saladInfoTime).fork
+    teaBrew <- waterBoil.join.zip(brewTea(teaBrewingTime)).fork
+    result <- eggsFry.zip(saladMaker).zip(teaBrew).join
+  } yield Map("eggs" -> result._1, "saladWithSourCream" -> result._2, "water" -> result._3._1, "tea" -> result._3._2)
+
+  private def fryEggs(eggsFiringTime: Duration): ZIO[Any, Throwable, LocalDateTime] = for {
+    _ <- ZIO.sleep(eggsFiringTime)
+  } yield LocalDateTime.now()
+
+  private def boilWater(waterBoilingTime: Duration): ZIO[Any, Throwable, LocalDateTime] = for {
+    _ <- ZIO.sleep(waterBoilingTime)
+  } yield LocalDateTime.now()
 
 
+  private def makeSalad(saladInfoTime: SaladInfoTime): ZIO[Any, Throwable, LocalDateTime] = for {
+    _ <- ZIO.sleep(saladInfoTime.tomatoTime).awaitAllChildren.zip(ZIO.sleep(saladInfoTime.cucumberTime))
+  } yield LocalDateTime.now()
+
+  private def brewTea(teaBrewingTime: Duration): ZIO[Any, Throwable, LocalDateTime] = for {
+    _ <- ZIO.sleep(teaBrewingTime)
+  } yield LocalDateTime.now()
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ZIO.succeed(println("Done"))
 
--- homeworks/homework_7/src/main/scala/ResuourceTraining.scala
+++ homeworks/homework_7/src/main/scala/ResuourceTraining.scala
@@ -3,6 +3,7 @@ package ru.dru
 import zio.{IO, Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
 
 import java.io.{BufferedReader, BufferedWriter, FileReader, FileWriter}
+import java.util.stream.Collectors
 
 
 /**
@@ -13,9 +14,19 @@ import java.io.{BufferedReader, BufferedWriter, FileReader, FileWriter}
 
 object ResuourceTraining extends ZIOAppDefault {
 
-  def readData(filePath: String): IO[Throwable, String] = ???
+  def readData(filePath: String): IO[Throwable, String] = ZIO.acquireReleaseWith(acquireReader(filePath))(releaseReader) {
+    reader => ZIO.succeed(reader.lines().collect(Collectors.joining("\n")))
+  }
 
-  def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = ???
+  private def acquireReader(filePath: String) = ZIO.attempt(new BufferedReader(new FileReader(filePath)))
+
+  private def acquireWriter(filePath: String) = ZIO.attempt(new BufferedWriter(new FileWriter(filePath)))
+  private def releaseReader(reader: BufferedReader) = ZIO.succeed(reader.close())
+
+  private def releaseWriter(writer: BufferedWriter) = ZIO.succeed(writer.close())
+  def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = ZIO.acquireReleaseWith(acquireWriter(filePath))(releaseWriter) {
+    writer => ZIO.attempt(writer.write(data))
+  }.orDie
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ZIO.succeed("Done")
 }
