518d09e40cff0756da83bfe0fb49487aba10599f
Ильдар Юмагулов <iyumagulov@naumen.ru>
2023-12-17 13:50:48 +0500

    homework_7

--- homeworks/homework_7/src/main/scala/Breakfast.scala
+++ homeworks/homework_7/src/main/scala/Breakfast.scala
@@ -1,7 +1,7 @@
 package ru.dru
 
 import zio.CanFail.canFailAmbiguous1
-import zio.{Duration, Exit, Fiber, Scope, ZIO, ZIOApp, ZIOAppArgs, ZIOAppDefault, durationInt}
+import zio.{Duration, Exit, Fiber, Scope, ZIO, ZIOApp, ZIOAppArgs, ZIOAppDefault, durationLong}
 
 import java.time.LocalDateTime
 import scala.concurrent.TimeoutException
@@ -32,9 +32,30 @@ object Breakfast extends ZIOAppDefault {
   def makeBreakfast(eggsFiringTime: Duration,
                     waterBoilingTime: Duration,
                     saladInfoTime: SaladInfoTime,
-                    teaBrewingTime: Duration): ZIO[Any, Throwable, Map[String, LocalDateTime]] = ???
+                    teaBrewingTime: Duration): ZIO[Any, Throwable, Map[String, LocalDateTime]] = {
+    val totalTimeSalad = durationLong(saladInfoTime.cucumberTime.toSeconds + saladInfoTime.tomatoTime.toSeconds)
 
+    for {
+      startTime <- ZIO.succeed(LocalDateTime.now())
+      eggsFryFiber <- ZIO.sleep(eggsFiringTime).as(plusDuration(startTime, eggsFiringTime)).fork
+      waterBoilFiber <- ZIO.sleep(waterBoilingTime).as(plusDuration(startTime, waterBoilingTime)).fork
+      saladWithSourCreamTime <- ZIO.sleep(totalTimeSalad.seconds).as(plusDurations(startTime, saladInfoTime.cucumberTime, saladInfoTime.tomatoTime))
+      waterBoiledTime <- waterBoilFiber.join
+      teaTime <- ZIO.sleep(teaBrewingTime).as(plusDuration(waterBoiledTime, teaBrewingTime))
+      eggsFriedTime <- eggsFryFiber.join
+    } yield Map(
+      "eggs" -> eggsFriedTime,
+      "water" -> waterBoiledTime,
+      "saladWithSourCream" -> saladWithSourCreamTime,
+      "tea" -> teaTime
+    )
+  }
 
+  private def plusDuration(time: LocalDateTime, duration: Duration): LocalDateTime =
+    time.plusSeconds(duration.toSeconds)
+
+  private def plusDurations(time: LocalDateTime, durations: Duration*): LocalDateTime =
+    durations.foldLeft(time)(plusDuration)
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ZIO.succeed(println("Done"))
 
