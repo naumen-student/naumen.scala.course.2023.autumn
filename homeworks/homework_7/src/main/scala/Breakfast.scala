c2de660979e6094d885c009f666199fd3e48e927
v.d.zubkov <v.d.zubkov@tinkoff.ru>
2023-12-21 13:29:03 +0500

    homework_7 - Зубков Владислав

--- homeworks/homework_7/src/main/scala/Breakfast.scala
+++ homeworks/homework_7/src/main/scala/Breakfast.scala
@@ -1,10 +1,8 @@
 package ru.dru
 
-import zio.CanFail.canFailAmbiguous1
-import zio.{Duration, Exit, Fiber, Scope, ZIO, ZIOApp, ZIOAppArgs, ZIOAppDefault, durationInt}
+import zio.{Clock, Duration, Fiber, Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
 
-import java.time.LocalDateTime
-import scala.concurrent.TimeoutException
+import java.time.{LocalDateTime, OffsetDateTime}
 
 case class SaladInfoTime(tomatoTime: Duration, cucumberTime: Duration)
 
@@ -15,24 +13,42 @@ object Breakfast extends ZIOAppDefault {
    * Функция должна эмулировать приготовление завтрака. Продолжительные операции необходимо эмулировать через ZIO.sleep.
    * Правила приготовления следующие:
    *  1. Нобходимо вскипятить воду (время кипячения waterBoilingTime)
-   *  2. Параллельно с этим нужно жарить яичницу eggsFiringTime
-   *  3. Параллельно с этим готовим салат:
-   *    * сначала режим  огурцы
-   *    * после этого режим помидоры
-   *    * после этого добавляем в салат сметану
-   *  4. После того, как закипит вода необходимо заварить чай, время заваривания чая teaBrewingTime
-   *  5. После того, как всё готово, можно завтракать
+   *     2. Параллельно с этим нужно жарить яичницу eggsFiringTime
+   *     3. Параллельно с этим готовим салат:
+   *     * сначала режим  огурцы
+   *     * после этого режим помидоры
+   *     * после этого добавляем в салат сметану
+   *     4. После того, как закипит вода необходимо заварить чай, время заваривания чая teaBrewingTime
+   *     5. После того, как всё готово, можно завтракать
    *
-   * @param eggsFiringTime время жарки яичницы
+   * @param eggsFiringTime   время жарки яичницы
    * @param waterBoilingTime время кипячения воды
-   * @param saladInfoTime информация о времени для приготовления салата
-   * @param teaBrewingTime время заваривания чая
+   * @param saladInfoTime    информация о времени для приготовления салата
+   * @param teaBrewingTime   время заваривания чая
    * @return Мапу с информацией о том, когда завершился очередной этап (eggs, water, saladWithSourCream, tea)
    */
   def makeBreakfast(eggsFiringTime: Duration,
                     waterBoilingTime: Duration,
                     saladInfoTime: SaladInfoTime,
-                    teaBrewingTime: Duration): ZIO[Any, Throwable, Map[String, LocalDateTime]] = ???
+                    teaBrewingTime: Duration): ZIO[Any, Throwable, Map[String, LocalDateTime]] = {
+
+    val totalTimeSalad = saladInfoTime.cucumberTime plus saladInfoTime.tomatoTime
+
+    for {
+      startTime <- ZIO.succeed(LocalDateTime.now())
+      eggsFryFiber <- ZIO.sleep(eggsFiringTime).as(startTime plus eggsFiringTime).fork
+      waterBoilFiber <- ZIO.sleep(waterBoilingTime).as(startTime plus waterBoilingTime).fork
+      saladWithSourCreamTime <- ZIO.sleep(totalTimeSalad).as(startTime plus totalTimeSalad)
+      waterBoiledTime <- waterBoilFiber.join
+      teaTime <- ZIO.sleep(teaBrewingTime).as(waterBoiledTime plus teaBrewingTime)
+      eggsFriedTime <- eggsFryFiber.join
+    } yield Map(
+      "eggs" -> eggsFriedTime,
+      "water" -> waterBoiledTime,
+      "saladWithSourCream" -> saladWithSourCreamTime,
+      "tea" -> teaTime
+    )
+  }
 
 
 
