0a964ae056ea55e0740e83e06baf311366a2ae15
lenapau <L_1111_P@mail.ru>
2023-12-22 14:35:43 +0500

    homework_7

--- homeworks/homework_7/src/main/scala/ResuourceTraining.scala
+++ homeworks/homework_7/src/main/scala/ResuourceTraining.scala
@@ -5,17 +5,25 @@ import zio.{IO, Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
 import java.io.{BufferedReader, BufferedWriter, FileReader, FileWriter}
 
 
-/**
- * Необходимо реализовать функции readData и writeData, записывающие и читающие данные в/из файла соответственно.
- * В реализации следует применять безопасное использование ресурсов ZIO.acquireReleaseWith
- */
-
 
 object ResuourceTraining extends ZIOAppDefault {
 
-  def readData(filePath: String): IO[Throwable, String] = ???
+  def readData(filePath: String): IO[Throwable, String] = {
+    ZIO.acquireReleaseWith(ZIO.attempt(
+      new BufferedReader(new FileReader(filePath))))(data => ZIO.attempt(data.close()).orDie)(
+      data => ZIO.attempt(data.readLine()))
+  }
 
-  def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = ???
+  def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = {
+    ZIO.acquireReleaseWith(ZIO.attempt(new BufferedWriter(new FileWriter(filePath))))(
+      writer => ZIO.attempt(writer.close()).orDie
+    )(
+      writer => ZIO.attempt {
+        writer.write(data)
+        writer.flush()
+      }
+    ).orDie
+  }
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ZIO.succeed("Done")
 }
